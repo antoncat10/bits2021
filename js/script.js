f3f418382b48a433efe9b23052142895bef5fc45
voxards <80433673+voxards@users.noreply.github.com>
2021-10-19 14:44:15 +1100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -142,7 +142,7 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
+// Close the dropdown if the user's mouse is not over it
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
   var myDropdown = document.getElementById("myDropdown");
